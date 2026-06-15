8bea73a680a54670055b38b4b79742b26bbf8894
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-06-15 12:29:12 +0530

    aded debug

--- vars/servicesPipeline.groovy
+++ vars/servicesPipeline.groovy
@@ -104,7 +104,7 @@ stage('Unit Tests') {
     stage('Quality Gate')
     {
         steps{
-            timeout(time: 10, unit: 'MINUTES') {
+            timeout(time: 20, unit: 'MINUTES') {
                 waitForQualityGate abortPipeline: true
 
                 sh """
